9e63471e92a4491d50fd2817fb1987618bf80628
Mohammed  kahlout <85851570+MohammedALaa-98@users.noreply.github.com>
2022-06-20 12:22:48 +0300

    first commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,11 @@
+<br>
+<footer class="bg-info text-center text-white">
+    <!-- Copyright -->
+    <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+            © 2022 Copyright:
+            <a class="text-white" href="#">mr:mak</a>
+          </div>
+    </div>
+  </footer>
+  <!-- Footer -->
